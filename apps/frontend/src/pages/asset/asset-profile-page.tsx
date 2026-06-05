3ee81153c530af4056507be7f4b49755e205c85d
Tomek <tomek@agileforce.pl>
2026-06-05 11:13:59 +0200

    feat(asset): show avg cost reference line on price chart

--- apps/frontend/src/pages/asset/asset-profile-page.tsx
+++ apps/frontend/src/pages/asset/asset-profile-page.tsx
@@ -1286,6 +1286,7 @@ export const AssetProfilePage = () => {
                   totalGainAmount={profile.totalGainAmount}
                   totalGainPercent={profile.totalGainPercent}
                   quoteHistory={quoteHistory ?? []}
+                  avgCost={symbolHolding?.averagePrice ?? undefined}
                   className={`col-span-1 ${symbolHolding ? "md:col-span-2" : "md:col-span-3"}`}
                 />
                 {symbolHolding && (
@@ -1313,6 +1314,7 @@ export const AssetProfilePage = () => {
                   totalGainAmount={profile.totalGainAmount}
                   totalGainPercent={profile.totalGainPercent}
                   quoteHistory={quoteHistory ?? []}
+                  avgCost={symbolHolding?.averagePrice ?? undefined}
                   className={`col-span-1 ${symbolHolding ? "md:col-span-2" : "md:col-span-3"}`}
                 />
                 {symbolHolding && (
